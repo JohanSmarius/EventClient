cf02c891a5d97b6f53742b30597f87da2d37db90
johansmarius <jsmarius@hotmail.com>
2024-02-17 14:43:48 +0100

    First version

---
+++ EventClient/Models/Event.cs
@@ -0,0 +1,16 @@
+namespace EventClient.Models;
+
+public class Event
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+    
+    public DateTime StartTime { get; set; }
+
+    public DateTime EndTime { get; set; }
+
+    public string Location { get; set; }
+}
